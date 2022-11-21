c82184ae3c8ee754f5721a815dbeeaeb13dd6e8b
Bishal Rai <bishal6193@gmail.com>
2022-11-21 15:50:57 +0545

    Basic Sign up Structure added

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div>
+            <div className="">
+                <h1>SIGN UP YOUR ACCOUNT</h1>
+            </div>
+            <div className="">
+                <div>
+                    <label>User Name</label>
+                    <input type="text" placeholder="User Name" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" placeholder="Password" />
+                    <label>Re-Enter Password</label>
+                    <input type="password" placeholder="Confirm Password" />
+                </div>
+                <button type="submit" className=''>Sign Up</button>
+            </div>
+            <a href="./Login">
+                <p>Log In</p>
+            </a>
+        </div>
+    )
 }
 export default Signup;
