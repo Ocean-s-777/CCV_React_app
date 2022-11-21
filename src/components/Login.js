9b40c4747a833079d52aeb66d9079c48bc96bf69
Bishal Rai <bishal6193@gmail.com>
2022-11-21 15:28:53 +0545

    Basic login structure added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Test Login</div>
-  )
+    return (
+        <div className="">
+            <h1>Sign into Your Account</h1>
+            <div className="">
+                <form action="#" method="post">
+                    <div>
+                        <input type="text" name="user" placeholder="Email Address" className="input-email" />
+                    </div>
+                    <div>
+                        <input type="password" name="password" id="" placeholder="Password" className="input-password" />
+                    </div>
+                    <a href="#">
+                        <p>Forgot Password?</p>
+                    </a>
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 export default Login;
