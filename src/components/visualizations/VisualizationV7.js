844c3e0768113a4bbfafda8d076511fa9b7c652a
raosk <oskari.rantamaula@gmail.com>
2022-11-28 12:48:29 +0200

    V7 WIP

--- src/components/visualizations/VisualizationV7.js
+++ src/components/visualizations/VisualizationV7.js
@@ -11,8 +11,8 @@ import { useState, useEffect } from "react";
 // Common attributes of graphs/lines/plots
 const BORDERWIDTH = 2;
 const POINTRADIUS = 0;
-const COLOR1 = "red";
-const COLOR2 = "blue";
+const COLOR1 = "blue";
+const COLOR2 = "tomato";
 
 // If run on localhost, asume localhost server is also used
 let currentURL = window.location.href;
@@ -23,10 +23,10 @@ let fetchURL = isDev
 fetchURL = "https://oceans777.herokuapp.com"; // Disable this line to benefit from the code above
 
 // Function to build datasets (from json) for a Line
-const buildDataset = (label, data, color, x, y, hidden) => ({
+const buildDataset = (label, data, color, x, y, hidden, yAxis) => ({
   label,
   data: data.map((d) => ({
-    time: Math.round((d[x])/1000), // Math.Round IS HERE JUST FOR TESTING PURPOSES, it should be removed as soon as we fix the related issue.
+    time: d[x],
     value: d[y],
   })),
   borderColor: color,
@@ -38,6 +38,7 @@ const buildDataset = (label, data, color, x, y, hidden) => ({
   borderWidth: BORDERWIDTH,
   pointRadius: POINTRADIUS,
   hidden,
+  yAxisID: yAxis
 });
 
 const VisualizationV7 = () => {
@@ -54,14 +55,18 @@ const VisualizationV7 = () => {
             json.v6_800k,
             COLOR1,
             "Time",
-            "co2_ppm"
+            "co2_ppm",
+            false,
+            "y"
           ),
           buildDataset(
             "surface temperature change",
             json.v7_2m,
             COLOR2,
             "Time",
-            "50%"
+            "50%",
+            false,
+            "y2"
           ),
         ],
       };
@@ -92,16 +97,25 @@ const VisualizationV7 = () => {
     },
     scales: {
       x: {
-        type: "time",
-        time: {
-          unit: "year",
-          unitStepSize: 1000  // Uncaught Error: -8620885906789000 and 978300000000 are too far apart with stepSize of 1 year. ???
-        },
+        type: "linear",
+        ticks: {
+          stepSize: 10000
+        }
       },
       y: {
         type: "linear",
         display: true,
         position: 'left',
+        title: {
+          display: true,
+          text: 'co2 ppm',
+          color: COLOR1,
+          font: {
+            size: 16,
+            family: '"Times New Roman", Times, serif'
+          },
+          reverse: true
+        }
       },
       y2: {
         type: "linear",
@@ -109,7 +123,18 @@ const VisualizationV7 = () => {
         position: 'right',
         grid: {
           drawOnChartArea: false,
-        }  
+        },
+        suggestedMin: -1,
+        suggestedMax: 1,
+        title: {
+          display: true,
+          text: 'surface temperature change',
+          color: COLOR2,
+          font: {
+            size: 16,
+            family: '"Times New Roman", Times, serif'
+          }
+        }
       },
     },
   };
