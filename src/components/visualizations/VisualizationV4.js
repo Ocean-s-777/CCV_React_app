e080f4ceea7e1e75a260029999a10b59ce0d80a0
Selina Kallio <selina.kallio@gmail.com>
2022-12-04 22:48:06 +0200

    v4: shortened text descriptions

--- src/components/visualizations/VisualizationV4.js
+++ src/components/visualizations/VisualizationV4.js
@@ -22,42 +22,42 @@ const dummyData = [
   {
     time: "1346",
     value: 300,
-    text: "bubonic plague caused by the Black Death bacillus Yersinia pestis kills a third of the human population across much of Europe, 1346-53"
+    text: "bubonic plague caused by the Black Death bacillus Yersinia pestis kills a third of the human population"
   },
   {
     time: "1542",
     value: 300,
-    text: "global population of humans passes 500 million; annual energy use per person averages 9,800 kWh, 14x the resting metabolism"
+    text: "global population of humans passes 500 million; annual energy use per person averages 9800 kWh"
   },
   {
     time: "1886",
     value: 300,
-    text: "first car with gasoline-powered internal combustion engine (Karl Benz, Germany, 1886) -> 97 million motor vehicles produced globally per year by 2017: peak production?"
+    text: "first car with gasoline-powered internal combustion engine (Karl Benz, Germany, 1886)"
   },
   {
     time: "1900",
     value: 300,
-    text: "global average life expectancy equals 32 years by 1900 -> doubling over the next 75 years, exposing diseases of ageing"
+    text: "global average life expectancy equals 32 years by 1900"
   },
   {
     time: "1918",
     value: 300,
-    text: "Spanish flu pandemic (1918-20): H1N1 influenza virus infects a third of the global population and kills 50-100 million, mostly in the 2nd wave"
+    text: "Spanish flu pandemic (1918-20): infects a third and kills 50-100 million"
   },
   {
     time: "1973",
     value: 300,
-    text: "global average life expectancy exceeds 60 years by 1973 -> 70 years by 2008 and rising for all countries; strengthening link to affluence, which drives down natural capital"
+    text: "global average life expectancy exceeds 60 years by 1973"
   },
   {
     time: "1986",
     value: 300,
-    text: "global population of humans passes 5 billion; annual energy use per person averages 18300 kWh, 26x the resting metabolism"
+    text: "global population of humans passes 5 billion; annual energy use per person averages 18300 kWh"
   },
   {
     time: "2017",
     value: 300,
-    text: "first national legislation for a mid-century target of net-zero emissions (Sweden, 2017) -> Suriname and Bhutan CO2-negative by 2019"
+    text: "first national legislation for a mid-century target of net-zero emissions (Sweden, 2017)"
   },
 ]
 
