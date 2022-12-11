9d6ced6af19d6b7357f6871afbb7101b1416773a
raosk <oskari.rantamaula@gmail.com>
2022-12-12 01:42:14 +0200

    Responsive scaling for graphs, narrow screens forced to 1 column

--- src/components/pages/Custom.js
+++ src/components/pages/Custom.js
@@ -1,43 +1,20 @@
 import React, { Component } from "react";
-
 import ChosenGraphs from "./modules/ChosenGraphs";
 
-//const fetchURL = "https://oceans777.herokuapp.com";
-
-// Dummy data
-// Will be removed when the real data can be fetched
-// Feel free to experiment by trying out different values
-// let customViewDummyData = {
-//   _id: "123123",
-//   id: "456456",
-//   title: "My very own custom view",
-//   columns: 2,
-//   visualizations: ["V7", "V3", "V9", "V5"],
-//   descriptions: [
-//     "This is a custom text for the FIRST graph shown on the custom view",
-//     null,
-//     "This is a custom text for the THIRD graph shown on the custom view",
-//     "This is a custom text for the FOURTH graph shown on the custom view",
-//   ],
-//   owner: "789789",
-// };
-
 export default function Custom(customViewData) {
-  let json = customViewData; // Will be replaced with a fetch function & real data
+  let aaaClassName = "visualizations OneColumnView";
+  if (customViewData.columns == 2) {
+    aaaClassName = "visualizations TwoColumnView";
+  }
 
-  const style = {
-    display: "grid",
-    gridTemplateColumns: `repeat(${json.columns}, 1fr)`,
-    gap: "16px",
-  };
   return (
     <>
       <div className="page_title_container">
-        <h1 className="page_title">{json.title}</h1>
+        <h1 className="page_title">{customViewData.title}</h1>
       </div>
       <br />
-      <div style={style} className="visualizations">
-        <ChosenGraphs data={json} />
+      <div className={aaaClassName}>
+        <ChosenGraphs data={customViewData} />
       </div>
     </>
   );
