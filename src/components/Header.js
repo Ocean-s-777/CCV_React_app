4ccd88fe306bacb2acc5e5abd0d0c0880be5fbc4
OxCone1 <t1lava01@students.oamk.fi>
2022-12-13 06:04:07 +0200

    Added logout if logged in to the top bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from './images/logo.png'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import createGrn from './images/createGreen.svg'
 import userGrn from './images/userGreen.svg'
-import LoginView from './pages/Login'
-import signupView from './pages/Signup'
-import PageSelect from './PageSelect'
+import logoutPic from './images/logout.png'
+import { UserAuthContext } from '../Context'
+
 
 export default function Header() {
+  const UserAuthContextValue = useContext(UserAuthContext);
+
+  function logout() {
+    UserAuthContextValue.logout();
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-head">
       <div className="container-fluid head">
         <div className="title-cont">
-          <img src={logo} alt={"logo"} href="/" />
-          <a className="navbar-brand title" href="/">Climate Change Visualiser</a></div>
+          <img src={logo} alt={"logo"} />
+          <NavLink className="navbar-brand title" to="/">Climate Change Visualiser</NavLink>
+        </div>
         <div className="icons">
-          <a href="/create"><img src={createGrn} alt={"add"}/></a>
-          <a href="/user"><img src={userGrn} alt={"user"}/></a>
-          {/* <img src={createGrn} alt={"add"} href="/Login"/>
-      <img src={userGrn} alt={"user"} href="/User"/> */}
+          <NavLink to={"/create"}> <img src={createGrn} alt={"add"} /></NavLink>
+          <NavLink to={"/user"}> <img src={userGrn} alt={"user"} /></NavLink>
+          {UserAuthContextValue.jwt != null ? <NavLink onClick={() => logout()}><img id='logout' src={logoutPic} alt={"logout"} /></NavLink> : <></>}
+
         </div>
       </div>
     </nav>
